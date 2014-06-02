2f6daa0dd8f72a30841e2866186c6a8e89c12ca2
Zsolt Rozsnyai <zsolt@pioneers.hu>
2014-06-02 17:12:48 +0200

    make formelements available for public

--- index.js
+++ index.js
@@ -2,4 +2,11 @@
 
 module.exports = {
   generate: require('./lib/generate'),
+  form: require('./lib/form'),
+  input: require('./lib/input'),
+  radios: require('./lib/radios'),
+  select: require('./lib/select'),
+  checkbox: require('./lib/checkbox'),
+  submit: require('./lib/submit'),
+  textarea: require('./lib/textarea')
 };
